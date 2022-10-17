0d3d3a17e922a0108cd3d854e481a4cf60b0e556
ezenbhy <ezenbhy@gmail.com>
2022-10-17 12:41:22 +0900

    first commit

---
+++ hooks/Info.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+
+function Info() {
+    const [name , setName] = useState('');
+    const [nickname , setNickname] = useState('');
+
+    const onChangeName = e => {
+        setName(e.target.value);
+    }
+    const onChangeNickname = e => {
+        setNickname(e.target.value);
+    }
+
+    useEffect(()=>{
+        console.log("랜더링이 완료되었습니다  componentDidMount()");
+        console.log({name,nickname}); 
+    },[name,nickname]);//componentDidUpdate()
+
+  return (
+    <div>
+        <div>
+            <input value={name} onChange={onChangeName}/>
+            <input value={nickname} onChange={onChangeNickname}/>
+        </div>
+        <div>
+            <div> <b>이름:</b> {name} </div>
+            <div> <b>닉네임:</b> {nickname} </div>
+        </div>
+    </div>
+  )
+}
+
+export default Info
